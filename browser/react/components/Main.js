3390c0bb14770e87fff91865e866a5a9e1e6bed6
Howard Kier <howard@Howards-MacBook-Air.local>
2017-10-11 11:30:10 -0500

    stopping point for review

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -6,16 +6,45 @@ import AllArtists from './AllArtists';
 import SingleArtist from './SingleArtist';
 import Sidebar from './Sidebar';
 import Player from './Player';
-import NewPlayList from './NewPlayList'
+import NewPlayList from './NewPlayList';
+import axios from 'axios'
+
 
 export default class Main extends Component {
 
-  render () {
+  constructor() {
+    super();
+    this.state = {
+      playlists: []
+    };
+    this.addPlaylist = this.addPlaylist.bind(this)
+  }
+
+  componentDidMount() {
+    axios.get('/api/playlists')
+      .then(res => res.data)
+      .then(playlists => this.setState({
+        playlists
+      })
+      );
+  }
+
+  addPlaylist (playlistName) {
+    axios.post('/api/playlists', { name: playlistName })
+      .then(res => res.data)
+      .then(playlist => {
+        this.setState({
+          playlists: [...this.state.playlists, playlist]
+        });
+      });
+  }
+
+  render() {
     return (
       <Router>
         <div id="main" className="container-fluid">
           <div className="col-xs-2">
-            <Sidebar />
+            <Sidebar playlists={this.state.playlists}/>
           </div>
           <div className="col-xs-10">
             <Switch>
@@ -23,13 +52,13 @@ export default class Main extends Component {
               <Route path="/albums/:albumId" component={SingleAlbum} />
               <Route exact path="/artists" component={AllArtists} />
               <Route path="/artists/:artistId" component={SingleArtist} />
-              <Route exact path="/newplaylist" component = {NewPlayList} />
+              <Route exact path="/NewPlayList" render={() => <NewPlayList addPlaylist={this.addPlaylist}/>}/>
               <Route component={StatefulAlbums} />
             </Switch>
           </div>
           <Player />
         </div>
-    </Router>
+      </Router>
     );
   }
 }
--- browser/react/components/NewPlayList.js
+++ browser/react/components/NewPlayList.js
@@ -3,24 +3,40 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 
+
+
 export default class NewPlaylist extends Component {
 
     constructor() {
         super();
         this.state = {
-            inputValue: []
+            inputValue: '',
+            disableSubmit: true,
+            dirty: false
         };
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
-        this.setState({ inputValue: event.target.value });
+        let input = event.target.value
+        this.setState({ inputValue: input });
+        this.setState({ disableSubmit: (input.length > 16 || input.length < 1) })
+        this.setState({ dirty: true })
     }
 
+    // verifySubmit() {
+    // }
+
+
     handleSubmit(event) {
-        console.log(this.state)
+        event.preventDefault()
+        this.props.addPlaylist(this.state.inputValue)
+        this.setState({ inputValue: "" })
+        this.setState({ dirty: false })
     }
+
+
 
 
     // componentDidMount() {
@@ -35,16 +51,22 @@ export default class NewPlaylist extends Component {
                 <form className="form-horizontal" onSubmit={this.handleSubmit}>
                     <fieldset>
                         <legend>New Playlist</legend>
+                        {this.state.dirty && this.state.inputValue.length > 16 &&
+                            (<div className="alert alert-warning"   >Please shorten the name</div>)
+                        }
+                        {this.state.dirty && this.state.inputValue.length === 0 &&
+                            (<div className="alert alert-warning"   >Please enter a name</div>)
+                        }
                         <div className="form-group">
                             <label className="col-xs-2 control-label">Name</label>
                             <div className="col-xs-10">
-                                <input className="form-control" type="text"
+                                <input className="form-control" type="text" value={this.state.inputValue}
                                     onChange={this.handleChange} />
                             </div>
                         </div>
                         <div className="form-group">
                             <div className="col-xs-10 col-xs-offset-2">
-                                <button type="submit" className="btn btn-success">Create Playlist
+                                <button id="playlistsubmit" type="submit" className="btn btn-success" disabled={this.state.disableSubmit}>Create Playlist
                                     </button>
                             </div>
                         </div>
